62fb0703f1e99e81bfa4ad4b188d0b9dc9fcec70
Brandon <vang.brandon94@gmail.com>
2023-09-01 08:09:43 -0700

    Fixed error

--- react-app/src/components/Likes/Dislike.js
+++ react-app/src/components/Likes/Dislike.js
@@ -1,57 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPostLikes, thunkAddLike, thunkRemoveLike, fetchUserLikes } from "../../store/like";
-import { fetchAllPosts, fetchFollowingPosts, getFollowingPosts } from '../../store/post';
-import LikeButton from "./Button";
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchPostLikes, thunkAddLike, thunkRemoveLike, fetchUserLikes } from "../../store/like";
+// import { fetchAllPosts, fetchFollowingPosts, getFollowingPosts } from '../../store/post';
+// import LikeButton from "./Button";
 
 
-const DisLikes = ({ post }) => {
-    const dispatch = useDispatch();
-    const userLikes = useSelector((state) => state.likes.userLikes);
-    const loggedInUserId = useSelector((state) => state.session.user && state.session.user.id);
-    // const post = useSelector((state) => state.posts.postId)
+// const DisLikes = ({ post }) => {
+//     const dispatch = useDispatch();
+//     const userLikes = useSelector((state) => state.likes.userLikes);
+//     const loggedInUserId = useSelector((state) => state.session.user && state.session.user.id);
+//     // const post = useSelector((state) => state.posts.postId)
 
 
-    let originalLikeId = null
-    post.likes.forEach(like => {
-        if (like.user_id === loggedInUserId) {
-            originalLikeId = like.id
-        }
-    })
-    const [likeId, setLikeId] = useState(originalLikeId || null);
+//     let originalLikeId = null
+//     post.likes.forEach(like => {
+//         if (like.user_id === loggedInUserId) {
+//             originalLikeId = like.id
+//         }
+//     })
+//     const [likeId, setLikeId] = useState(originalLikeId || null);
 
-    // useEffect(() => {
-    //     dispatch(fetchPostLikes(post.id));
-    // }, [dispatch, post.id]);
+//     // useEffect(() => {
+//     //     dispatch(fetchPostLikes(post.id));
+//     // }, [dispatch, post.id]);
 
-    const isUserLiked = () => {
-        return userLikes.some((like) => like.post_id === post.id);
-    };
+//     const isUserLiked = () => {
+//         return userLikes.some((like) => like.post_id === post.id);
+//     };
 
-    const handleLike = async () => {
-        if (isUserLiked()) {
-            await dispatch(thunkRemoveLike(likeId));
-        } else {
-            const data = await dispatch(thunkAddLike(post.id));
-            setLikeId(data.id);
-        }
+//     const handleLike = async () => {
+//         if (isUserLiked()) {
+//             await dispatch(thunkRemoveLike(likeId));
+//         } else {
+//             const data = await dispatch(thunkAddLike(post.id));
+//             setLikeId(data.id);
+//         }
 
-        await Promise.all([
-            dispatch(fetchUserLikes(loggedInUserId)),
-            dispatch(fetchFollowingPosts()),
-            dispatch(fetchAllPosts())
-        ]);
-    };
+//         await Promise.all([
+//             dispatch(fetchUserLikes(loggedInUserId)),
+//             dispatch(fetchFollowingPosts()),
+//             dispatch(fetchAllPosts())
+//         ]);
+//     };
 
-    return (
-        <div>
-            {loggedInUserId && (
-                <div className="likes-container">
-                    <LikeButton isLiked={isUserLiked()} onLike={handleLike} />
-                </div>
-            )}
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             {loggedInUserId && (
+//                 <div className="likes-container">
+//                     <LikeButton isLiked={isUserLiked()} onLike={handleLike} />
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
 
-export default DisLikes;
+// export default DisLikes;
--- react-app/src/components/Likes/DislikeButton.js
+++ react-app/src/components/Likes/DislikeButton.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import "./Like.css";
+// import React, { useState, useEffect } from "react";
+// import "./Like.css";
 
-const DisLikeButton = ({ isLiked, onLike }) => {
-    const [liked, setLiked] = useState(isLiked);
-    const iconColor = liked ? "blue" : "white";
+// const DisLikeButton = ({ isLiked, onLike }) => {
+//     const [liked, setLiked] = useState(isLiked);
+//     const iconColor = liked ? "blue" : "white";
 
-    const handleClick = () => {
-        setLiked((prevLiked) => !prevLiked);
-        onLike();
-    };
+//     const handleClick = () => {
+//         setLiked((prevLiked) => !prevLiked);
+//         onLike();
+//     };
 
-    return (
-        <div className="like-button" onClick={handleClick}>
-            <i className="fa fa-arrow-down" style={{ color: iconColor }} />
-        </div>
-    );
-};
+//     return (
+//         <div className="like-button" onClick={handleClick}>
+//             <i className="fa fa-arrow-down" style={{ color: iconColor }} />
+//         </div>
+//     );
+// };
 
-export default DisLikeButton;
+// export default DisLikeButton;
