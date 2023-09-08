de4a493ba4612eb45bf62e83bd1ed66ac55cf46c
Brandon <vang.brandon94@gmail.com>
2023-09-08 15:06:14 -0700

    Fixed error

--- app/api/dislikes_routes.py
+++ app/api/dislikes_routes.py
@@ -5,35 +5,35 @@ from flask_login import login_required,current_user
 
 dislike_routes = Blueprint('dislikes', __name__)
 
-@like_routes.route('/<int:post_id>',methods=['GET'])
+@dislike_routes.route('/<int:post_id>',methods=['GET'])
 def get_post_likes(post_id):
     '''
     get post's likes
     '''
-    post_likes = Like.query.filter_by(post_id=post_id).all()
-    response_post_likes=[like.to_dict() for like in post_likes]
-    return {"likes":response_post_likes}
+    post_dislikes = Dislike.query.filter_by(post_id=post_id).all()
+    response_post_dislikes=[dislike.to_dict() for dislike in post_dislikes]
+    return {"dislikes":response_post_dislikes}
 
-@like_routes.route('/users/<int:user_id>',methods=['GET'])
+@dislike_routes.route('/users/<int:user_id>',methods=['GET'])
 def get_specific_user_likes(user_id):
     '''
     get specific user likes
     '''
-    user_likes = Like.query.filter_by(user_id=user_id).all()
-    response_user_likes=[like.to_dict() for like in user_likes]
-    return {"likes":response_user_likes}
+    user_dislikes = Dislike.query.filter_by(user_id=user_id).all()
+    response_user_dislikes=[dislike.to_dict() for dislike in user_dislikes]
+    return {"dislikes":response_user_dislikes}
 
-@like_routes.route('/users/current',methods=['GET'])
+@dislike_routes.route('/users/current',methods=['GET'])
 @login_required
 def get_user_likes():
     '''
     get users likes
     '''
-    user_likes = Like.query.filter_by(user_id=current_user.id).all()
-    response_user_likes=[like.to_dict() for like in user_likes]
-    return {"likes":response_user_likes}
+    user_dislikes = Dislike.query.filter_by(user_id=current_user.id).all()
+    response_user_dislikes=[dislike.to_dict() for dislike in user_dislikes]
+    return {"dislikes":response_user_dislikes}
 
-@like_routes.route('/<int:post_id>/likes',methods=['POST'])
+@dislike_routes.route('/<int:post_id>/likes',methods=['POST'])
 @login_required
 def add_like(post_id):
     '''
@@ -43,26 +43,26 @@ def add_like(post_id):
     if 'errors' in auth:
         return auth
 
-    already_liked = Like.query.filter_by(post_id=post_id, user_id=auth['id']).first()
-    if already_liked:
+    already_disliked = Dislike.query.filter_by(post_id=post_id, user_id=auth['id']).first()
+    if already_disliked:
         return {'error':'You already liked this post'}
 
-    new_like = Like(user_id=auth['id'], post_id=post_id)
-    db.session.add(new_like)
+    new_dislike = Dislike(user_id=auth['id'], post_id=post_id)
+    db.session.add(new_dislike)
     db.session.commit()
-    return new_like.to_dict()
+    return new_dislike.to_dict()
 
-@like_routes.route('/<int:likeid>',methods=['DELETE'])
+@dislike_routes.route('/<int:likeid>',methods=['DELETE'])
 @login_required
-def remove_like(likeid):
+def remove_like(dislikeid):
     '''
     unlike
     '''
-    like_delete = Like.query.get(likeid)
+    dislike_delete = Dislike.query.get(dislikeid)
 
-    if like_delete.users.id != current_user.id:
+    if dislike_delete.users.id != current_user.id:
         return jsonify({'error': 'You are not authorized to unlike'}), 401
 
-    db.session.delete(like_delete)
+    db.session.delete(dislike_delete)
     db.session.commit()
-    return {"message": "Like Successfully deleted"}
+    return {"message": "DisLike Successfully deleted"}
